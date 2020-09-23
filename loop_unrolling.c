5a33eaeb7dc75d0232442a66756b9e0af0cc3503
Artem Kovera <artemkovera@gmail.com>
2020-09-23 17:40:11 +0300

    made modifications to program for loop unrolling

--- loop_unrolling.c
+++ loop_unrolling.c
@@ -77,13 +77,19 @@ double multiplyWith5xLoopUnrolling(const double array[], const size_t size)
 
 int main ()
 {
-    clock_t start1, end1, start2, end2, start3, end3, start4, end4;
-    clock_t start3x1, end3x1, start3x2, end3x2, start3x3, end3x3, start3x4, end3x4;
-    clock_t start5x1, end5x1, start5x2, end5x2, start5x3, end5x3, start5x4, end5x4;  
+    struct timespec start1, end1, start2, end2, start3, end3, start4, end4;
+    struct timespec start3x1, end3x1, start3x2, end3x2, start3x3, end3x3, start3x4, end3x4;
+    struct timespec start5x1, end5x1, start5x2, end5x2, start5x3, end5x3, start5x4, end5x4;  
 
-    const size_t SIZE = 200;
+    double tdiff1, tdiff2, tdiff3, tdiff4, tdiff3x1, tdiff3x2, tdiff3x3, tdiff3x4,
+    tdiff5x1, tdiff5x2, tdiff5x3, tdiff5x4;
+
+    double result;
+
+    const size_t SIZE = 1000000;
+    const size_t SIZE2 = 1000;
         
-    double arr [] = {[0 ... 999999] = 1.00001}; //works only with gcc
+    static double arr [] = {[0 ... 999999] = 1.00001}; //works only with gcc
 
     double arr2 [] = {
                         1.1, 2.3, 3,4, 0.6, 1.4, 2.2, 0.02, 2.7, 
@@ -110,72 +116,188 @@ int main ()
                         0.2, 0.3, 0.1, 4.5, 8.2, 9.3, 5.1, 9.0, 
                         0.4, 8.2, 0.1, 9.4, 0.4, 0.3, 8.7, 3.3,
                         5.7, 8.2, 7.3, 0.4, 0.03, 0.2, 9.5, 9.2,
-                        8.1, 1.3, 3.2, 0.4   
+                        8.1, 1.3, 3.2, 0.4, 1.1, 2.3, 3,4, 0.6,
+                        1.4, 0.4, 2, 0.2, 0.5, 3, 0.01, 0.34, 2, 
+                        2.3, 3.4, 5.5, 5.3, 5.4, 7.4, 1.4, 0.77,
+                        2.1, 3.4, 1.1, 3.0, 0.4, 0.1, 0.2, 0.9,
+                        0.3, 9.2, 4.2, 0.2, 0.3, 2.1, 3.2, 1.01,
+                        0.01, 0.2, 2.3, 7.2, 9.1, 0.4, 4.2, 0.3,
+                        9.2, 1.3, 3.2, 8.1, 3.1, 9.1, 0.2, 0.02,
+                        0.03, 0.1, 9.2, 2.1, 3.4, 9.1, 0.3, 0.2,
+                        0.2, 0.3, 0.1, 4.5, 8.2, 9.3, 5.1, 9.0, 
+                        0.4, 8.2, 0.1, 9.4, 0.4, 0.3, 8.7, 3.3,
+                        5.7, 8.2, 7.3, 0.4, 0.03, 0.2, 9.5, 9.2,
+                        0.7, 0.2, 0.02, 5.4, 8.1, 1.3, 3.2, 0.4,
+                        1.2, 0.02, 1.2, 0.02, 0.7, 0.2, 7.2, 5.4,
+                        1.1, 2.3, 3,4, 0.6, 1.4, 2.2, 0.02, 2.7, 
+                        1.4, 0.4, 2, 0.2, 0.5, 3, 0.01, 0.34, 2, 
+                        2.3, 3.4, 5.5, 5.3, 5.4, 7.4, 1.4, 0.77,
+                        2.1, 3.4, 1.1, 3.0, 0.4, 0.1, 0.2, 0.9,
+                        0.3, 9.2, 4.2, 0.2, 0.3, 2.1, 3.2, 1.01,
+                        0.01, 0.2, 2.3, 7.2, 9.1, 0.4, 4.2, 0.3,
+                        9.2, 1.3, 8.2, 8.1, 3.1, 9.1, 0.2, 0.02,
+                        0.03, 0.1, 1.2, 2.1, 3.4, 9.1, 0.3, 0.2,
+                        0.2, 0.3, 0.1, 4.5, 8.2, 9.3, 5.1, 9.0, 
+                        0.4, 1.2, 0.1, 0.4, 0.4, 0.3, 1.7, 3.3,
+                        5.7, 1.2, 7.3, 0.4, 0.03, 0.2, 9.5, 0.2,
+                        8.1, 1.3, 3.2, 0.4, 1.4, 2.2, 0.02, 2.7, 
+                        1.1, 2.1, 3,4, 0.6, 1.4, 2.2, 0.02, 2.7, 
+                        1.4, 0.4, 2, 0.2, 0.5, 3, 0.01, 0.34, 2, 
+                        2.3, 3.4, 5.5, 5.3, 5.4, 7.4, 1.4, 0.77,
+                        2.1, 3.4, 1.1, 3.0, 0.4, 0.1, 0.2, 0.9,
+                        0.3, 3.2, 1.2, 0.2, 0.3, 2.1, 3.2, 1.01,
+                        0.01, 0.2, 0.3, 7.2, 9.1, 0.4, 4.2, 0.3,
+                        9.2, 1.3, 8.2, 8.1, 3.1, 9.1, 0.2, 0.02,
+                        0.03, 0.1, 9.2, 2.1, 3.4, 9.1, 0.3, 0.2,
+                        0.2, 0.3, 0.1, 4.5, 8.2, 9.3, 5.1, 9.0, 
+                        0.4, 0.2, 0.1, 7.4, 0.4, 0.3, 8.7, 3.3,
+                        5.7, 0.2, 0.3, 0.4, 0.03, 0.2, 9.5, 9.2,
+                        0.7, 0.2, 7.2, 5.4, 8.1, 1.3, 3.2, 0.4,
+                        1.2, 0.02, 1.2, 0.02, 0.7, 0.2, 7.2, 5.4,
+                        1.1, 2.3, 1,4, 0.6, 1.4, 2.2, 0.02, 2.7, 
+                        1.4, 0.4, 2, 0.2, 0.5, 3, 0.01, 0.34, 2, 
+                        2.3, 3.4, 5.5, 5.3, 5.4, 7.4, 1.4, 0.77,
+                        2.1, 3.4, 1.1, 3.0, 0.4, 0.1, 0.2, 0.9,
+                        0.3, 9.2, 4.2, 0.2, 0.3, 2.1, 3.2, 1.01,
+                        0.01, 0.2, 2.3, 7.2, 2.1, 0.4, 4.2, 0.3,
+                        9.2, 1.3, 8.2, 8.1, 3.1, 0.1, 0.2, 0.02,
+                        0.03, 0.1, 9.2, 2.1, 3.4, 1.1, 0.3, 0.2,
+                        0.2, 0.3, 0.1, 4.5, 8.2, 9.3, 5.1, 9.0, 
+                        0.4, 8.2, 0.1, 9.4, 0.4, 0.3, 8.7, 3.3,
+                        2.7, 8.2, 7.3, 0.4, 0.03, 0.2, 9.5, 9.2,
+                        8.1, 1.3, 3.2, 0.4, 1.1, 2.3, 3,4, 0.6,
+                        1.4, 0.4, 2, 0.2, 0.5, 3, 0.01, 0.34, 2, 
+                        2.3, 3.4, 2.5, 3.3, 5.4, 7.4, 1.4, 0.77,
+                        2.1, 3.4, 1.1, 3.0, 0.4, 0.1, 0.2, 0.9,
+                        0.3, 9.2, 4.2, 0.2, 0.3, 2.1, 3.2, 1.01,
+                        0.01, 0.2, 2.3, 7.2, 9.1, 0.4, 4.2, 0.3,
+                        9.2, 1.3, 8.2, 8.1, 3.1, 9.1, 0.2, 0.02,
+                        0.03, 0.1, 0.2, 2.1, 3.4, 9.1, 0.3, 0.2,
+                        0.2, 0.3, 0.1, 4.5, 8.2, 9.3, 5.1, 9.0, 
+                        0.4, 0.01, 0.1, 9.4, 0.4, 0.3, 8.7, 3.3,
+                        5.7, 0.2, 7.3, 0.4, 0.03, 0.2, 9.5, 9.2,
+                        0.7, 0.2, 3.2, 5.4, 0.01, 1.3, 3.2, 0.4,
+                        1.2, 0.02, 1.2, 0.02, 0.7, 0.2, 7.2, 5.4,
+                        1.1, 2.3, 3,4, 0.6, 1.4, 2.2, 0.02, 2.7, 
+                        1.4, 0.4, 2, 0.2, 0.5, 3, 0.01, 0.34, 2, 
+                        2.3, 3.4, 5.5, 5.3, 5.4, 7.4, 1.4, 0.77,
+                        2.1, 3.4, 1.1, 3.0, 0.4, 0.1, 0.2, 0.9,
+                        0.3, 0.2, 4.2, 0.2, 0.3, 2.1, 3.2, 1.01,
+                        0.01, 0.2, 2.3, 7.2, 9.1, 0.4, 4.2, 0.3,
+                        0.02, 1.3, 0.2, 8.1, 3.1, 9.1, 0.2, 0.02,
+                        0.03, 0.1, 9.2, 2.1, 3.4, 9.1, 0.3, 0.2,
+                        0.2, 0.3, 0.1, 4.5, 8.2, 9.3, 5.1, 9.0, 
+                        0.4, 5.2, 0.1, 6.4, 0.4, 0.3, 8.7, 3.3,
+                        1.7, 4.2, 7.3, 0.4, 0.03, 0.2, 9.5, 9.2,
+                        0.1, 1.3, 3.2, 0.4, 1.4, 2.2, 0.02, 2.7,
+                        1.1, 2.3, 1,4, 0.6, 1.4, 2.2, 0.02, 2.7, 
+                        1.4, 0.4, 2, 0.2, 0.5, 3, 0.01, 0.34, 2, 
+                        2.3, 3.4, 2.25, 5.3, 5.4, 7.4, 1.4, 0.77,
+                        2.1, 3.4, 1.1, 3.0, 0.4, 0.1, 0.2, 0.9,
+                        0.3, 1.2, 3.2, 0.2, 0.3, 2.1, 3.2, 1.01,
+                        0.01, 0.2, 2.3, 7.2, 9.1, 0.4, 4.2, 0.3,
+                        0.2, 1.3, 3.2, 8.1, 3.1, 0.1, 0.2, 0.02,
+                        0.03, 0.1, 9.2, 2.1, 3.4, 0.1, 0.3, 0.2,
+                        0.2, 0.3, 0.1, 4.5, 8.2, 9.3, 5.1, 9.0, 
+                        0.4, 8.2, 0.1, 9.4, 0.4, 0.3, 8.7, 3.3,
+                        5.7, 8.2, 7.3, 0.4, 0.03, 0.2, 9.5, 9.2,
+                        0.7, 0.2, 7.2, 5.4, 8.1, 1.3, 3.2, 0.4,
+                        1.2, 0.02, 1.2, 1.1, 2.3, 3,4, 0.6, 1.4, 
+                        1.4, 0.4, 2.02, 0.2, 0.5, 3, 0.01, 0.34, 2, 
+                        2.3, 3.4, 5.5, 5.3, 5.4, 7.4, 1.4, 0.77,
+                        2.1, 3.4, 1.1, 3.0, 0.4, 0.1, 0.2, 0.9,
+                        0.3, 9.2, 4.2, 0.2, 0.3, 2.1, 3.2, 1.01,
+                        1.01, 0.2, 2.3, 7.2, 9.1, 0.4, 4.2, 0.3,
+                        9.2, 1.3, 8.2, 8.1, 3.1, 0.1, 0.2, 0.02,
+                        0.03, 0.1, 9.2, 2.1, 3.4, 0.1, 0.3, 0.2,
+                        0.2, 0.3, 0.1, 4.5, 8.2, 0.3, 5.1, 9.0, 
+                        0.4, 8.2, 0.1, 4.4, 0.4, 0.3, 0.7, 3.3,
+                        0.7, 8.2, 7.3, 0.4, 0.03, 0.2, 0.5, 9.2,
+                        0.7, 0.2, 1.2, 2.4, 3.1, 1.3, 3.2, 0.4,
+                        1.2, 0.02, 1.2, 1.2, 0.02, 2.7
                      };
 
 
     printf("Calling functions with first set of data: \n");
-    start1 = clock();
-    mult(arr, 1000000);
-    end1 = clock();
-    printf("the number of clock ticks elapsed for function with NO loop unrolling - %li\n", end1 - start1);
+    clock_gettime(CLOCK_MONOTONIC, &start1);
+    mult(arr, SIZE);
+    clock_gettime(CLOCK_MONOTONIC, &end1);
+    tdiff1 = (end1.tv_sec - start1.tv_sec) + 1e-9*(end1.tv_nsec - start1.tv_nsec);
+    printf("  %f  seconds elapsed  for function with NO loop unrolling \n", tdiff1);
     
-    start3x1 = clock();
-    multWithLoop3xUnrolling(arr, 1000000);
-    end3x1 = clock();
-    printf("the number of clock ticks elapsed for function with 3x loop unrolling   - %li\n", end3x1 - start3x1);
-
-    start5x1 = clock();
-    multiplyWith5xLoopUnrolling(arr, 1000000);
-    end5x1 = clock();
-    printf("the number of clock ticks elapsed for function with 5x loop unrolling   - %li\n", end5x1 - start5x1);  
-
-    start2 = clock();
-    mult(arr, 1000000);
-    end2 = clock();
-    printf("the number of clock ticks elapsed for function with NO loop unrolling - %li (second check)\n", end2 - start2);
     
-    start3x2 = clock();
-    multWithLoop3xUnrolling(arr, 1000000);
-    end3x2 = clock();
-    printf("the number of clock ticks elapsed for function with 3x loop unrolling   - %li (second check)\n", end3x2 - start3x2);
+    clock_gettime(CLOCK_MONOTONIC, &start3x1);
+    multWithLoop3xUnrolling(arr, SIZE);
+    clock_gettime(CLOCK_MONOTONIC, &end3x1);
+    tdiff3x1 = (end3x1.tv_sec - start3x1.tv_sec) + 1e-9*(end3x1.tv_nsec - start3x1.tv_nsec);
+    printf("  %f  seconds elapsed for function with 3x loop unrolling\n", tdiff3x1);
 
-    start5x2 = clock();
-    multiplyWith5xLoopUnrolling(arr, 1000000);
-    end5x2 = clock();
-    printf("the number of clock ticks elapsed for function with 5x loop unrolling   - %li (second check)\n", end5x2 - start5x2);        
+    
+    clock_gettime(CLOCK_MONOTONIC, &start5x1);
+    multiplyWith5xLoopUnrolling(arr, SIZE);
+    clock_gettime(CLOCK_MONOTONIC, &end5x1);
+    tdiff5x1 = (end5x1.tv_sec - start5x1.tv_sec) + 1e-9*(end5x1.tv_nsec - start5x1.tv_nsec);
+    printf("  %f  seconds elapsed for function with 5x loop unrolling\n", tdiff5x1);  
+
+
+    clock_gettime(CLOCK_MONOTONIC, &start2);
+    mult(arr, SIZE);
+    clock_gettime(CLOCK_MONOTONIC, &end2);
+    tdiff2 = (end2.tv_sec - start2.tv_sec) + 1e-9*(end2.tv_nsec - start2.tv_nsec);
+    printf("  %f  seconds elapsed for function with NO loop unrolling (second check)\n", tdiff2);
+    
+    clock_gettime(CLOCK_MONOTONIC, &start3x2);
+    multWithLoop3xUnrolling(arr, SIZE);
+    clock_gettime(CLOCK_MONOTONIC, &end3x2);
+    tdiff3x2 = (end3x2.tv_sec - start3x2.tv_sec) + 1e-9*(end3x2.tv_nsec - start3x2.tv_nsec);
+    printf("  %f  seconds elapsed for function with 3x loop unrolling (second check)\n", tdiff3x2);
+
+    clock_gettime(CLOCK_MONOTONIC, &start5x2);
+    multiplyWith5xLoopUnrolling(arr, SIZE);
+    clock_gettime(CLOCK_MONOTONIC, &end5x2);
+    tdiff5x2 = (end5x2.tv_sec - start5x2.tv_sec) + 1e-9*(end5x2.tv_nsec - start5x2.tv_nsec);
+    printf("  %f  seconds elapsed for function with 5x loop unrolling (second check)\n", tdiff5x2);        
 
 
     printf("\nCalling functions with another set of data: \n");
-    start3 = clock();
-    mult(arr2, SIZE);
-    end3 = clock();
-    printf("the number of clock ticks elapsed for function with NO loop unrolling - %li\n", end3 - start3);
     
-    start3x3 = clock();
-    multWithLoop3xUnrolling(arr2, SIZE);
-    end3x3 = clock();
-    printf("the number of clock ticks elapsed for function with 3x loop unrolling   - %li\n", end3x3 - start3x3);
-
-    start5x3 = clock();
-    multiplyWith5xLoopUnrolling(arr2, SIZE);
-    end5x3 = clock();
-    printf("the number of clock ticks elapsed for function with 5x loop unrolling   - %li\n", end5x3 - start5x3);  
-
-    start4 = clock();
-    mult(arr2, SIZE);
-    end4 = clock();
-    printf("the number of clock ticks elapsed for function with NO loop unrolling - %li (second check)\n", end4 - start4);
+    clock_gettime(CLOCK_MONOTONIC, &start3);
+    result = mult(arr2, SIZE2);
+    clock_gettime(CLOCK_MONOTONIC, &end3);
+    tdiff3 = (end3.tv_sec - start3.tv_sec) + 1e-9*(end3.tv_nsec - start3.tv_nsec);
+    printf("  %f  seconds elapsed for function with NO loop unrolling | result = %e\n", tdiff3, result);
     
-    start3x4 = clock();
-    multWithLoop3xUnrolling(arr2, SIZE);
-    end3x4 = clock();
-    printf("the number of clock ticks elapsed for function with 3x loop unrolling   - %li (second check)\n", end3x4 - start3x4);
+    clock_gettime(CLOCK_MONOTONIC, &start3x3);
+    result = multWithLoop3xUnrolling(arr2, SIZE2);
+    clock_gettime(CLOCK_MONOTONIC, &end3x3);
+    tdiff3x3 = (end3x3.tv_sec - start3x3.tv_sec) + 1e-9*(end3x3.tv_nsec - start3x3.tv_nsec);
+    printf("  %f seconds elapsed for function with 3x loop unrolling | result = %e\n", tdiff3x3, result);
 
-    start5x4 = clock();
-    multiplyWith5xLoopUnrolling(arr2, SIZE);
-    end5x4 = clock();
-    printf("the number of clock ticks elapsed for function with 5x loop unrolling   - %li (second check)\n", end5x4 - start5x4);            
+    clock_gettime(CLOCK_MONOTONIC, &start5x3);
+    result = multiplyWith5xLoopUnrolling(arr2, SIZE2);
+    clock_gettime(CLOCK_MONOTONIC, &end5x3);
+    tdiff5x3 = (end5x3.tv_sec - start5x3.tv_sec) + 1e-9*(end5x3.tv_nsec - start5x3.tv_nsec);
+    printf("  %f seconds elapsed for function with 5x loop unrolling | result = %e\n", tdiff5x3, result);
     
+    
+    clock_gettime(CLOCK_MONOTONIC, &start4);
+    result = mult(arr2, SIZE2);
+    clock_gettime(CLOCK_MONOTONIC, &end4);
+    tdiff4 = (end4.tv_sec - start4.tv_sec) + 1e-9*(end4.tv_nsec - start4.tv_nsec);
+    printf("  %f  seconds elapsed for function with NO loop unrolling (second check) | result = %e\n", tdiff4, result);
+    
+    clock_gettime(CLOCK_MONOTONIC, &start3x4);
+    result = multWithLoop3xUnrolling(arr2, SIZE2);
+    clock_gettime(CLOCK_MONOTONIC, &end3x4);
+    tdiff3x4 = (end3x4.tv_sec - start3x4.tv_sec) + 1e-9*(end3x4.tv_nsec - start3x4.tv_nsec);
+    printf("  %f  seconds elapsed for function with 3x loop unrolling (second check) | result = %e\n", tdiff3x4, result);
+
+    clock_gettime(CLOCK_MONOTONIC, &start5x4);
+    result = multiplyWith5xLoopUnrolling(arr2, SIZE2);
+    clock_gettime(CLOCK_MONOTONIC, &end5x4);
+    tdiff5x4 = (end5x4.tv_sec - start5x4.tv_sec) + 1e-9*(end5x4.tv_nsec - start5x4.tv_nsec);
+    printf("  %f  seconds elapsed for function with 5x loop unrolling (second check) | result = %e\n", tdiff5x4, result);          
+    
+
     return 0;
 }
